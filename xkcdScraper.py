118d67860db2cdcb1bb04391b3f4cc8e37b03c0c
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2018-02-28 14:48:55 -0700

    Update xkcdScraper.py

--- xkcdScraper.py
+++ xkcdScraper.py
@@ -23,7 +23,7 @@ while not url.endswith('#'):
         res = requests.get(comicUrl)
         res.raise_for_status()
 
-    # Save the image to ./xkcd
+    # Save the image to ./xkcd folder
     imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
     for chunk in res.iter_content(100000):
         imageFile.write(chunk)
