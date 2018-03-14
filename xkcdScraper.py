185c58bf46d57664fe333d4b2ed20f284eadb246
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2018-03-14 10:22:35 -0600

    Update xkcdScraper.py

--- xkcdScraper.py
+++ xkcdScraper.py
@@ -9,7 +9,6 @@ while not url.endswith('#'):
     print('Downloading page %s...' % url)
     res = requests.get(url)
     res.raise_for_status()
-
     soup = bs4.BeautifulSoup(res.text)
 
     # Find the URL of the Comic Images
@@ -33,4 +32,4 @@ while not url.endswith('#'):
     prevLink = soup.select('a[rel="prev"]')[0]
     url = 'http://xkcd.com' + prevLink.get('href')
 
-print('Done.')
+print('[+] Images Saved to xkcd Folder')
